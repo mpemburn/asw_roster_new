8d0307e07fc5b27f964675db3a51660720f75468
Mark Pemburn <mark@blueoceanideas.net>
2020-04-12 15:47:49 -0400

    Add @method doc block to RosterAuth facade

--- app/Facades/RosterAuth.php
+++ app/Facades/RosterAuth.php
@@ -8,8 +8,18 @@ use Illuminate\Support\Facades\Facade;
  * Class RosterAuth
  * @package App\Facades
  *
- * @method getMemberName
  * @method getMemberId
+ * @method getMemberName
+ * @method getUserCoven
+ * @method grantRoleToUser
+ * @method isAdmin
+ * @method isCovenLeader($coven)
+ * @method isCovenScribe($coven)
+ * @method isElder
+ * @method isGuildLeader
+ * @method isMemberOf($roleName)
+ * @method isThisMember($memberId)
+ * @method revokeRoleFromUser($user, $roleName)
  * @method userIsLeaderOrScribe
  */
 class RosterAuth extends Facade
