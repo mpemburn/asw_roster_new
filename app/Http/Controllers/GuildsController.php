40220914dddc7a79d1600c8626d1e02d8b7cd222
Mark Pemburn <mark@blueoceanideas.net>
2020-04-13 08:17:23 -0400

    Clean up GuildsController

--- app/Http/Controllers/GuildsController.php
+++ app/Http/Controllers/GuildsController.php
@@ -4,106 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\GuildMember;
 use Illuminate\Http\Request;
-use App\Http\Requests;
 // Facades
-use GuildMembership;
-use Membership;
-use RosterAuth;
+use App\Facades\GuildMembership;
+use App\Facades\Membership;
+use App\Facades\RosterAuth;
 
 class GuildsController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
-    /**
      * Add or remove members
      */
-    public function manage($guild_id)
+    public function manage($guildId)
     {
-        $active = Membership::getGuildMembers($guild_id);
-        $has_leader_role = RosterAuth::isGuildLeader();
-        $member_id = RosterAuth::getMemberId();
-        $is_admin = RosterAuth::isAdmin();
-        $is_leader = GuildMembership::isLeader($guild_id, $member_id);
-        $can_edit = ($is_admin || ($has_leader_role && $is_leader));
+        $active = Membership::getGuildMembers($guildId);
+        $hasLeaderRole = RosterAuth::isGuildLeader();
+        $memberId = RosterAuth::getMemberId();
+        $isAdmin = RosterAuth::isAdmin();
+        $isLeader = GuildMembership::isLeader($guildId, $memberId);
+        $canEdit = ($isAdmin || ($hasLeaderRole && $isLeader));
 
-        return view('guild_manage', ['members' => $active, 'can_edit' => $can_edit]);
+        return view('guild_manage', ['members' => $active, 'can_edit' => $canEdit]);
     }
 
-    public function add(Request $request) {
+    public function add(Request $request): array
+    {
         $guild_member = new GuildMember();
         $guild_member->GuildID = $request->guild_id;
         $guild_member->MemberID = $request->member_id;
@@ -122,10 +46,10 @@ class GuildsController extends Controller
     }
 
     public function remove(Request $request) {
-        $guild_member = GuildMember::where('GuildID', $request->guild_id)
+        $guildMember = GuildMember::where('GuildID', $request->guild_id)
             ->where('MemberID', $request->member_id);
 
-        $guild_member->delete();
+        $guildMember->delete();
 
         return ['success' => true];
     }
